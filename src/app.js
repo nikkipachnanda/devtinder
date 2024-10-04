eb16121d17f3162e009b6464e62eddf65bf09d87
nitin <nitinpachnanda@gmail.com>
2024-10-04 21:56:09 +0530

    api changes

--- src/app.js
+++ src/app.js
@@ -2,14 +2,23 @@ const express = require("express");
 
 const app = express();
 
-app.use("/test",(req, res)=> {
-    res.send("Hello namaste from server");
-})
+// this will only handle get request call to user
+app.get("/user", (req, res)=> {
+    res.send({
+        "name":"Akshay",
+        "lastname":"kumar"
+    });
+   })
 
-app.use("/hi",(req, res)=> {
-    res.send("Hi from server"); 
-}) 
+   app.post("/user", (req, res)=> {
+    //sava data to db
+    res.send("Data saved to database");
+   })   
 
+// this will match all api calls (default api)   
+app.get("/", (req, res)=> {
+    res.send("namaste express node");
+   })   
 
 app.listen(5000, ()=> {
     console.log("Server is running");
